be2ded80912262c2701cd6e1bbf922120ce4550a
Joe Schmitt <joe@compass.com>
2018-05-18 17:50:21 -0400

    Implement pagination

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import {WebClient} from '@slack/client';
 
 import formatAttachment from './lib/formatAttachment.js';
 import getRequestData from './lib/getRequestData.js';
-import searchFinerGifs from './lib/searchFinerGifs.js';
+import searchFinerGifs, {MAX_RESULTS} from './lib/searchFinerGifs.js';
 
 const client = new WebClient();
 const clientId = process.env.SLACK_CLIENT_ID;
@@ -40,8 +40,24 @@ export default async function (event, context, callback) {
 
     if (requestData.type === 'url_verification') {
       return callback(null, {statusCode: 200, body: requestData.challenge});
-    } else if (requestData.type === 'interactive_message') {
+    }
+
+    if (requestData.type === 'interactive_message') {
       const [action] = requestData.actions;
+
+      if (requestData.callback_id === 'load_or_cancel') {
+        if (!action.value) {
+          await axios.post(requestData.response_url, {delete_original: true});
+          return callback(null, {statusCode: 200});
+        }
+
+        const value = JSON.parse(action.value);
+        const msg = await buildSearchResponseMsg(value.text, requestData.user.id, value.start);
+
+        await axios.post(requestData.response_url, msg);
+        return callback(null, {statusCode: 200});
+      }
+
       const value = JSON.parse(action.value);
       value.pretext = unescape(value.pretext);
       const message = {
@@ -53,7 +69,9 @@ export default async function (event, context, callback) {
 
       await axios.post(requestData.response_url, message);
       return callback(null, {statusCode: 200});
-    } else if (requestData.code) {
+    }
+
+    if (requestData.code) {
       const resp = await client.oauth.access({
         client_id: clientId,
         client_secret: clientSecret,
@@ -67,15 +85,14 @@ export default async function (event, context, callback) {
         },
         body: JSON.stringify(resp),
       });
-    } else if (requestData.token !== process.env.SLACK_VERIFICATION_TOKEN) {
+    }
+
+    if (requestData.token !== process.env.SLACK_VERIFICATION_TOKEN) {
       return callback(null, {statusCode: 403, body: 'Forbidden'});
     }
 
     if (requestData.text) {
-      const {tokens, tags} = hashtag.parse(requestData.text);
-      const query = tokens.reduce((val, tok) => val + (tok.text || ''), '').trim();
-
-      if (query === 'help') {
+      if (requestData.text === 'help') {
         return callback(null, {
           statusCode: 200,
           body: JSON.stringify({
@@ -85,32 +102,9 @@ export default async function (event, context, callback) {
         })
       }
 
-      const {results, hits} = await searchFinerGifs(query, tags);
-
-      if (!results.length) {
-        return callback(null, {
-          statusCode: 200,
-          body: JSON.stringify({
-            response_type: 'ephemeral',
-            text: `No results for ‘${query}’`,
-          }),
-        });
-      }
-
-      let response_type = 'in_channel';
-      if (tags.includes('select') || tags.includes('choose')) {
-        response_type = 'ephemeral';
-      }
-
       return callback(null, {
         statusCode: 200,
-        body: JSON.stringify({
-          response_type,
-          delete_original: response_type === 'in_channel',
-          attachments: results.slice(0, 5).map(({fields}) => {
-            return formatAttachment(query, fields, hits.found, results.length > 1, requestData);
-          }),
-        }),
+        body: JSON.stringify(await buildSearchResponseMsg(requestData.text, requestData.user_id)),
       });
     }
 
@@ -118,5 +112,56 @@ export default async function (event, context, callback) {
     callback(null, {statusCode: 200});
   } catch (e) {
     callback(e);
+  }
+}
+
+async function buildSearchResponseMsg(text, user_id, start = 0) {
+  const {tokens, tags} = hashtag.parse(text);
+  const query = tokens.reduce((val, tok) => val + (tok.text || ''), '').trim();
+
+  const {results, hits} = await searchFinerGifs(query, tags, start);
+
+  if (!query) {
+    return {
+      response_type: 'ephemeral',
+      text: 'Please enter a search term.',
+    };
+  }
+
+  if (!results.length) {
+    return {
+      response_type: 'ephemeral',
+      text: `No results for ‘${query}’.`,
+    };
+  }
+
+  let response_type = 'in_channel';
+  if (tags.includes('select') || tags.includes('choose')) {
+    response_type = 'ephemeral';
+  }
+
+  const attachments = results.slice(0, MAX_RESULTS).map(({fields}) => {
+    return formatAttachment(query, fields, hits.found, results.length > 1, user_id);
+  });
+
+  if (hits.found > MAX_RESULTS) {
+    const loadMoreVal = {text, start: start + MAX_RESULTS};
+
+    attachments.splice(attachments.length, 0, {
+      title: `${hits.found} total results`,
+      title_link: `${process.env.SITE_BASE_URL}?q=${query}`,
+      callback_id: 'load_or_cancel',
+      actions: [
+        {name: 'more', type: 'button', text: 'Load More', value: JSON.stringify(loadMoreVal)},
+        {name: 'cancel', type: 'button', style: 'danger', text: 'Cancel'},
+      ],
+    });
+  }
+
+  return {
+    response_type,
+    replace_original: start > 0,
+    delete_original: response_type === 'in_channel',
+    attachments,
   }
 }
